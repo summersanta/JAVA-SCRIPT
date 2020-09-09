3ffc0cfbd2d1d65f1bc8d7ca35c3a01beb2a32ae
Gyeongwon Jo <jessy1085@gmail.com>
2020-09-09 23:31:18 +0900

    9/9 최종

--- weather.js
+++ weather.js
@@ -1,10 +1,20 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "1011071b143eb55c6086febcb78811df";
 const COORDS = 'coords';
 
 function getWeather(lat, lng) {
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
-    );
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`;
+        });
 }
 
 function saveCoords(coordsObj) {
@@ -37,6 +47,7 @@ function loadCoords() {
         askForCoords();
     } else {
         const parsedCoords = JSON.parse(loadedCoords);
+
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
 
     }
